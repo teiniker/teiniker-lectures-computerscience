c29867410b63b07023b32e9a69e2e7301f87a004
teiniker <egon.teiniker@gmail.com>
2020-10-14 17:11:11 +0200

    Initial import.

---
+++ c-basics/selection/conditional_expression.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    int a = 7;      // User input       
+    int b = 13;     // User input
+
+    int max = a > b ? a : b;
+
+    printf("Max value is: %d\n", max);
+    
+	return 0;
+}
--- c-basics/selection/if_statement.c
+++ c-basics/selection/if_statement.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>    // since c99
 
 const int MIN = 0;
 const int MAX = 10;
@@ -15,6 +16,20 @@ int main(void)
     {
         printf("Value is invalid!\n");
     }
+
+    int a = 7;
+    int b = 13;
+
+    bool a_is_greater = a > b;  
+    if(a_is_greater)
+    {
+        printf("a is greater than b!\n");
+    }
+    else
+    {
+        printf("a is less or equal to b!\n");
+    }
     
+
 	return 0;
 }
