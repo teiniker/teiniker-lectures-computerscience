4580c1da91654a88611a1da5311ebcb742fd18d6
teiniker <egon.teiniker@gmail.com>
2020-10-20 20:08:06 +0200

    Initial import.

---
+++ c-basics/loops/do_while_loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 0;
+    do
+    {
+        printf("\t  i =%2d\n", i);
+        i++;
+    } while(i < 10);
+    
+    return 0;
+}
---
+++ c-basics/loops/while_loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 0;
+    while(i < 10)
+    {
+        printf("\t  i =%2d\n", i);
+        i++;
+    }
+    return 0;
+}
