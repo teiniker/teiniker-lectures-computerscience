9659238f7fb0bb394d484fd12b3a431ee4216d0e
Egon Teiniker <egon.teiniker@gmail.com>
2020-11-04 16:20:29 +0100

    Refactoring.

--- c-basics/exercises/leap-year/leap_year.c
+++ c-basics/exercises/leap-year/leap_year.c
@@ -4,8 +4,8 @@
 int main(void)
 {
     // initial values
-    //int yyyy = 1900;    // false
-    int yyyy = 1999;    // false
+    int yyyy = 1900;    // false
+    //int yyyy = 1999;    // false
     //int yyyy = 2000;    // true
     //int yyyy = 2020;    // true
 
@@ -13,22 +13,26 @@ int main(void)
 
     if(yyyy % 400 == 0)
     {
-        is_leap_year = true;        
-    }
-    else if(yyyy % 100 == 0)
-    {
-        is_leap_year = false;
-    }
-    else if(yyyy % 4 == 0)
-    {
         is_leap_year = true;
     }
     else
-    {
-        is_leap_year = false;
+    { 
+        if(yyyy % 100 == 0)
+        {
+            is_leap_year = false;
+        } 
+        else
+        { 
+            if(yyyy % 4 == 0)
+            {
+                is_leap_year = true;
+            }
+            else 
+            {
+                is_leap_year = false;
+            }
+        }
     }
- 
     printf("%4d : is_leap_year = %s\n", yyyy, is_leap_year ? "true" : "false");
-    
     return 0;
 }
