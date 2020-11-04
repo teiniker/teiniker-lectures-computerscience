a166c6610697c3700cdfcf9370d4a3040229b2a1
Egon Teiniker <egon.teiniker@gmail.com>
2020-11-04 11:12:47 +0100

    Refactoring.

--- c-basics/exercises/print-ascii-table/print_ascii.c
+++ c-basics/exercises/print-ascii-table/print_ascii.c
@@ -6,14 +6,9 @@ int main(void)
     printf("\t| char|  Dec  |  Hex  |  Oct |\n");
     printf("\t+-----+-------+-------+------+\n");
 
-    char c = 0;
-    while(c < 127)
+    for(char c = 32; c < 127; c++)
     {
-        if(c > 31 && c < 127 )
-        {
-            printf("\t  '%c'\t%3d\t0x%2x\t%3o\n", c, c, c, c);
-        }        
-        c++;
+        printf("\t  '%c'\t%3d\t0x%2x\t%3o\n", c, c, c, c);
     }
     return 0;
 }
