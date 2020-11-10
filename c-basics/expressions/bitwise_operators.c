e3e078d8699123c325ed69ed90ad0cb5cd1aaaef
teiniker <egon.teiniker@fh-joanneum.at>
2020-11-10 09:56:28 +0100

    Refactoring.

--- c-basics/expressions/bitwise_operators.c
+++ c-basics/expressions/bitwise_operators.c
@@ -5,17 +5,11 @@
 
 void print_binary(uint32_t number)
 {
-    char str[sizeof(number)*8+1];
-    for(int i=0; i < sizeof(number)*8; i++)
+     for(int i=sizeof(number)*8-1; i>= 0; i--)
     {
-        str[i] = (number & 1) ? '1' : '0';
-        number = number >> 1;
-    }
-    for(int i=sizeof(number)*8-1; i>= 0; i--)
-    {
-        putc(str[i], stdout);     
-    }
-    putc('\n',stdout);
+        putchar((number & 1UL << i) ? '1' : '0');
+    }    
+    putchar('\n');
 }
 
 
@@ -51,35 +45,6 @@ int main(void)
     print_binary(0xcccc);       // 00000000000000001100110011001100
                                 //                 0000000011111111     
     print_binary(0xcccc ^ 0xff);// 00000000000000001100110000110011
-    printf("\n");
-
-
-    // Arduino.h
-
-    printf("%d\n", bit(3));         // 8
-
-    print_binary(0xfedc);           // 00000000000000001111111011011100
-    print_binary(lowByte(0xfedc));  // 00000000000000000000000011011100
-    print_binary(highByte(0xfedc)); // 00000000000000000000000011111110
-    printf("\n");
-
-    uint8_t byte;
-    byte = 0xff;
-    bitClear(byte, 3);
-    print_binary(byte);     // 00000000000000000000000011110111
-    printf("\n");
-
-    byte = 0x00;
-    bitSet(byte, 3);
-    print_binary(byte);     // 00000000000000000000000000001000
-    printf("\n");
-
-    bool b = bitRead(0xcc, 2);
-    printf("%d\n", b);
-
-    byte = 0x00;
-    bitWrite(byte, 3, 1);
-    print_binary(byte);     // 00000000000000000000000000001000
     printf("\n");
 
     return 0;
--- c-basics/expressions/bitwise_operators_arduino.c
+++ c-basics/expressions/bitwise_operators_arduino.c
@@ -5,17 +5,11 @@
 
 void print_binary(uint16_t number)
 {
-    char str[sizeof(number)*8+1];
-    for(int i=0; i < sizeof(number)*8; i++)
-    {
-        str[i] = (number & 1) ? '1' : '0';
-        number = number >> 1;
-    }
     for(int i=sizeof(number)*8-1; i>= 0; i--)
     {
-        putc(str[i], stdout);     
-    }
-    putc('\n',stdout);
+        putchar((number & 1UL << i) ? '1' : '0');
+    }    
+    putchar('\n');
 }
 
 // ArduinoCore-avr/cores/arduino/Arduino.h
@@ -35,20 +29,20 @@ int main(void)
 
     printf("%d\n", bit(3));         // 8
 
-    print_binary(0xfedc);           // 00000000000000001111111011011100
-    print_binary(lowByte(0xfedc));  // 00000000000000000000000011011100
-    print_binary(highByte(0xfedc)); // 00000000000000000000000011111110
+    print_binary(0xfedc);           // 1111111011011100
+    print_binary(lowByte(0xfedc));  // 0000000011011100
+    print_binary(highByte(0xfedc)); // 0000000011111110
     printf("\n");
 
     uint8_t byte;
     byte = 0xff;
     bitClear(byte, 3);
-    print_binary(byte);     // 00000000000000000000000011110111
+    print_binary(byte);             // 0000000011110111
     printf("\n");
 
     byte = 0x00;
     bitSet(byte, 3);
-    print_binary(byte);     // 00000000000000000000000000001000
+    print_binary(byte);             // 0000000000001000
     printf("\n");
 
     bool b = bitRead(0xcc, 2);
@@ -56,7 +50,7 @@ int main(void)
 
     byte = 0x00;
     bitWrite(byte, 3, 1);
-    print_binary(byte);     // 00000000000000000000000000001000
+    print_binary(byte);             // 0000000000001000
     printf("\n");
 
     return 0;
