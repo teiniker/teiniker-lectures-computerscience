6593a137d517fc798d70ce5387423136c3607747
teiniker <egon.teiniker@fh-joanneum.at>
2020-11-17 10:15:13 +0100

    Initial import.

---
+++ c-basics/exercises/array-min-max-mean-exercise/array_min_max_mean.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define N 12
+
+int main(void)
+{
+    int values[N] = {-7, 0, 12, 6, -2, 23, 12, 9, -10, 19, -16, -1};
+
+    int min;
+    int max;
+    int sum;
+
+    // TODO
+    
+    return 0;
+}
---
+++ c-basics/exercises/array-min-max-mean/array_min_max_mean.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define N 12
+
+int main(void)
+{
+    int values[N] = {-7, 0, 12, 6, -2, 23, 12, 9, -10, 19, -16, -1};
+
+    int min = values[0];
+    int max = values[0];
+    int sum = 0;
+
+    for(int i=0; i<N; i++)
+    {
+        sum += values[i];
+
+        if(values[i] < min)
+            min = values[i];
+
+        if(values[i] > max)
+            max = values[i];
+    }
+    double mean = (double)sum/N;    // without (double) => 3.0000 !!!
+    printf("\tmin=%d\tmax=%d\tmean=%f\n", min, max, mean);
+
+    return 0;
+}
