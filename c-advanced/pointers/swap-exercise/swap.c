fef0b31502800eee45231321227742a17839f814
teiniker <egon.teiniker@gmail.com>
2021-02-17 21:46:35 +0100

    Initial import.

---
+++ c-advanced/pointers/swap-exercise/swap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <assert.h>
+
+void swap(int *a_ptr, int *b_ptr);
+
+int main(void)
+{
+    int a = 3;
+    int b = 7;
+    printf("a = %d \t b = %d\n", a, b);
+
+    swap(&a, &b);
+    printf("a = %d \t b = %d\n", a, b);
+    assert(a == 7);
+    assert(b == 3);
+
+    return 0;
+}
+
+// TODO
