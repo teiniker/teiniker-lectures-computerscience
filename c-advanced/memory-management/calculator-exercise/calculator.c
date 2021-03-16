7700734d1583696023a037f33498a26408c27898
teiniker <egon.teiniker@gmail.com>
2021-03-16 17:19:41 +0100

    Initial import.

---
+++ c-advanced/memory-management/calculator-exercise/calculator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    int b;
+    char op;
+
+
+    return 0;
+}
---
+++ c-advanced/memory-management/calculator/calculator.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    int b;
+    char op;
+
+    // read two integer numbers and a char from the console
+    printf("a b op > ");
+    scanf("%d %d %c", &a, &b, &op);
+
+    switch(op)
+    {
+        case '+':
+            printf("%d + %d = %d\n", a, b, a + b);
+            break;
+
+        case '-':
+            printf("%d - %d = %d\n", a, b, a - b);
+            break;
+
+        default:
+            printf("Operation not supported!\n");
+            break;    
+    }
+
+    return 0;
+}
