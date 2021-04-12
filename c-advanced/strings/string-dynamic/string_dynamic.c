93c86bcb85a04db3a87e82ad07fa5fa65da505af
teiniker <egon.teiniker@gmail.com>
2021-04-12 20:23:39 +0200

    Initial import.

---
+++ c-advanced/strings/string-dynamic/string_dynamic.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+    int str_len = 40;
+
+    // Dynamic allocated string buffer
+    char *d = malloc(str_len+1);
+    d[0] = 'A';
+    d[1] = 'B';
+    d[2] = 'C';
+    d[3] = '\0';
+    printf("\"%s\"\n", d);
+
+    // Release string buffer
+    free(d);
+
+    return 0;
+}
