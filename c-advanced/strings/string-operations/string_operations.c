1ebfc73c52036102c78584232988d8c2808d9be1
teiniker <egon.teiniker@gmail.com>
2021-04-19 21:39:00 +0200

    Refactoring.

--- c-advanced/strings/string-operations/string_operations.c
+++ c-advanced/strings/string-operations/string_operations.c
@@ -4,31 +4,38 @@
 #include <assert.h>
 
 size_t string_length(const char *s);
+bool string_equal(const char *s1, const char *s2);
 char *string_copy(const char *s);
 char *string_concatenation(const char *s1, const char *s2);
-bool string_equal(const char *s1, const char *s2);
 
 int main(void)
 {
-    // verify string_equal()
+    // Verify string_length()
+    assert(string_length("1001 0011 1111 0000") == 19); 
+
+    // Verify string_equal()
     assert(string_equal("1100", "1100"));   // s1 == a2
     assert(!string_equal("100", "1100"));   // s1 is too short
     assert(!string_equal("0100", "1100"));  // s1 != s2
 
-    // verify string_copy()
-    char *s = "1001 0011 1111 0000";
+    // Verify string_copy()
+    char *s = "1001 0011 1111 0000";    
     char *p = string_copy(s);
     printf("\"%s\" \n", p);
+    assert(string_equal("1001 0011 1111 0000", p));  
     free(p);
 
+    // Verify string_concatenation()
     char *s1 = "1001 0011 ";
     char *s2 = "1111 0000";
     char *q = string_concatenation(s1, s2);
     printf("\"%s\" \n", q);
+    assert(string_equal("1001 0011 1111 0000", q));  
     free(q);
 
     return 0;
 }
+
 
 size_t string_length(const char *s)
 {
