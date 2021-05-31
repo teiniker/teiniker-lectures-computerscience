2bce48c2c879ced7a415852a695cf3544af8004c
teiniker <egon.teiniker@gmail.com>
2021-05-31 22:34:20 +0200

    Initial import.

---
+++ data-structures/tree/binary-tree/binary_tree.h
@@ -0,0 +1,21 @@
+#ifndef _BINARY_TREE_H_
+#define _BINARY_TREE_H_
+
+#include <stdio.h>
+
+typedef struct node 
+{
+    int value;
+    struct node *left_ptr;
+    struct node *right_ptr;
+} node_t;
+
+extern node_t *node_new(int value);
+extern node_t *tree_insert_left(node_t *node_ptr, int value);
+extern node_t *tree_insert_right(node_t *node_ptr, int value);
+
+extern void tree_traversal_preorder(node_t *node_ptr);
+extern void tree_traversal_inorder(node_t *node_ptr);
+extern void tree_traversal_postorder(node_t *node_ptr);
+
+#endif /* _BINARY_TREE_H_ */
---
+++ data-structures/tree/binary-tree/binary_tree_test.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unity.h>
+
+#include "binary_tree.h"
+
+node_t *root_ptr = NULL;
+
+void setUp(void)
+{
+    root_ptr = node_new(13);
+    tree_insert_left(root_ptr, 7);
+    tree_insert_right(root_ptr, 23);
+
+    tree_insert_left(root_ptr->left_ptr, 3);
+    tree_insert_right(root_ptr->left_ptr, 11);
+
+    tree_insert_left(root_ptr->right_ptr, 17);
+    tree_insert_right(root_ptr->right_ptr, 29);
+
+}
+
+void tearDown(void)
+{
+    free(root_ptr->left_ptr->left_ptr);
+    free(root_ptr->left_ptr->right_ptr);
+    free(root_ptr->left_ptr);
+
+    free(root_ptr->right_ptr->left_ptr);
+    free(root_ptr->right_ptr->right_ptr);
+    free(root_ptr->right_ptr);
+
+    free(root_ptr);
+    root_ptr = NULL;
+}
+
+
+void test_traversal_preorder(void)
+{
+    printf("Pre-Order: ");
+    tree_traversal_preorder(root_ptr);
+    printf("\n");
+}
+
+void test_traversal_inorder(void)
+{
+    printf("In-Order: ");
+    tree_traversal_inorder(root_ptr);
+    printf("\n");
+}
+
+void test_traversal_postorder(void)
+{
+    printf("Post-Order: ");
+    tree_traversal_postorder(root_ptr);
+    printf("\n");
+}
+
+int main(void)
+{
+	UNITY_BEGIN();
+	RUN_TEST(test_traversal_preorder);
+    RUN_TEST(test_traversal_inorder);
+    RUN_TEST(test_traversal_postorder);
+	return UNITY_END();
+}
