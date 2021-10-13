0cf3ec23b6f2518dddb0d9623502fd737beb99d6
teiniker <egon.teiniker@fh-joanneum.at>
2021-10-13 01:57:36 -0700

    Initial import.

---
+++ c-basics/representations/number-types/number_types.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("char : %u\n", sizeof(char)); 
+    printf("short: %u\n", sizeof(short)); 
+    printf("int  : %u\n", sizeof(int)); 
+    printf("long : %u\n", sizeof(long)); 
+    
+    printf("float : %u\n", sizeof(float)); 
+    printf("double: %u\n", sizeof(double)); 
+  
+    return 0;
+}
