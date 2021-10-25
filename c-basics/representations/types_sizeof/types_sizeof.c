b26fc101896171f4055fa7d7a880f6dcb3e6eb6f
teiniker <egon.teiniker@gmail.com>
2021-10-25 22:38:47 +0200

    Refactoring.

--- c-basics/representations/types_sizeof/types_sizeof.c
+++ c-basics/representations/types_sizeof/types_sizeof.c
@@ -6,9 +6,13 @@ int main(void)
     printf("short: %u\n", sizeof(short)); 
     printf("int  : %u\n", sizeof(int)); 
     printf("long : %u\n", sizeof(long)); 
+    printf("long long: %u\n", sizeof(long long));
     
     printf("float : %u\n", sizeof(float)); 
     printf("double: %u\n", sizeof(double)); 
-  
+    printf("long double: %u\n", sizeof(long double));
+
+    printf("void* : %u\n", sizeof(void*));
+
     return 0;
 }
