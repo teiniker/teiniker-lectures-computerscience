e34d355079f372476ea6affd124115fe4325ff5f
teiniker <egon.teiniker@gmail.com>
2021-10-27 13:41:52 +0200

    Refactoring.

--- c-basics/expressions/bit-manipulation/bit_manipulation.c
+++ c-basics/expressions/bit-manipulation/bit_manipulation.c
@@ -13,6 +13,14 @@ int main(void)
     // Reads a bit of a number.
     value = 0b0000111100001111;
     bit = 3;
+
+    // 0b 0000 1111 0000 1111   >> 3
+ 
+    // 0b 0000 0001 1110 0001
+    // 0b 0000 0000 0000 0001  & 0x01
+    // ----------------------
+    // 0b 0000 0000 0000 0001 => bit 3 is set
+
     bool bit_3 = (value >> bit) & 0x01;
     printf("bit = %d\n", bit_3);
 
@@ -20,6 +28,10 @@ int main(void)
     // Sets (writes a 1 to) a bit of a numeric variable.
     value = 0b0000111100001111;
     bit = 5;
+    // 0b0000111100001111
+    // 0b0000000000100000  1 << 5; |
+    // ------------------
+    // 0b0000111100101111
     value = value | (1 << bit);
     printf("value = ");
     print_binary(value);
