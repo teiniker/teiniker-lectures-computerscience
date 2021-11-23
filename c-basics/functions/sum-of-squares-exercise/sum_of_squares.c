af5f534b14c2dda558086293f2463f215c901ebb
teiniker <egon.teiniker@gmail.com>
2021-11-23 19:40:38 +0100

    Initial import.

---
+++ c-basics/functions/sum-of-squares-exercise/sum_of_squares.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <assert.h>
+
+int sum_of_squares(int n);
+
+int main(void)
+{
+    int n=5;
+
+    int actual = sum_of_squares(n);
+    printf("Sum of squares from 1 to %d = %d\n", n, actual);
+    
+    int expected = n*(n+1)*(2*n+1)/6;    
+    assert(expected == actual); 
+
+    return 0;
+}
+
+// TODO: 
