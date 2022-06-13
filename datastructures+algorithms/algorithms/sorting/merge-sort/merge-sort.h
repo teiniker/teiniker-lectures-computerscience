7d22fa7eff1f88b3ef84cf467e2e6ac0d1acbdc3
teiniker <egon.teiniker@gmail.com>
2022-06-13 20:51:37 +0200

    Refactoring example to use make and unity.

---
+++ datastructures+algorithms/algorithms/sorting/merge-sort/merge-sort.h
@@ -0,0 +1,7 @@
+#ifndef _ARRAY_MERGE_SORT_H_
+#define _ARRAY_MERGE_SORT_H_
+
+extern void array_merge_sort(int data[], int n);
+extern void array_print(int data[], int size);
+
+#endif /* _ARRAY_MERGE_SORT_H_ */
