c859f1c07598f9d57ae53bca9093e8db62c86838
Egon Teiniker <egon.teiniker@gmail.com>
2022-11-01 22:16:36 +0100

    Initial import of a new exercise.

--- programming-c/c-basics/types-operators-expressions/expressions/resistor-exercise/resistor.c
+++ programming-c/c-basics/types-operators-expressions/expressions/resistor-exercise/resistor.c
@@ -6,11 +6,7 @@ const double ERROR = 1E-3;
 
 int main(void)
 {
-    double r1 = 100.0;
-    double r2 = 220.0;
-    double r3 = 330.0;
-
-    double r_ges = r1 + r2*r3/(r2+r3);
+    // TODO: Implement calculation
 
     printf("Rges = %5.2f Ohm\n", r_ges);
     assert(fabs(232.0 - r_ges) <= ERROR);
