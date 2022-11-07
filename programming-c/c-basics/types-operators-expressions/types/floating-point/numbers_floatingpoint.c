5d553c09efe62f77adc73374898ce77090777b44
Egon Teiniker <egon.teiniker@gmail.com>
2022-11-07 20:41:46 +0100

    Refactoring.

--- programming-c/c-basics/types-operators-expressions/types/floating-point/numbers_floatingpoint.c
+++ programming-c/c-basics/types-operators-expressions/types/floating-point/numbers_floatingpoint.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <math.h>
+
+void dump_memory(const void *s, size_t bytes); // just use it :-)
 
 int main(void)
 {
@@ -20,5 +23,21 @@ int main(void)
     printf("1.0/0.0  = %f\n", 1.0/0.0);
     printf("-1.0/0.0  = %f\n", -1.0/0.0);
 
+    float f =  -4.6015625f;
+    dump_memory(&f, sizeof(f));
+    // memory [0x7fffee3767b0]: 00 40 93 c0 !! little-endian 
+    // => 0xC0934000  
+
     return 0;
+}
+
+void dump_memory(const void *s, size_t bytes)
+{
+    printf("memory [%p]: ", s);
+    uint8_t *ptr = (uint8_t *)s;
+    for(size_t i=0; i<bytes; i++)
+    {
+        printf("%02x ", ptr[i]);
+    }
+    printf("\n");
 }
