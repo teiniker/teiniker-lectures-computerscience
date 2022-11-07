4042f8bb9717858b111bf08b9e9513f7b61e99cb
Egon Teiniker <egon.teiniker@gmail.com>
2022-11-07 20:29:24 +0100

    Refactoring.

--- programming-c/c-basics/types-operators-expressions/types/character/strings.c
+++ programming-c/c-basics/types-operators-expressions/types/character/strings.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+void dump_memory(const char *s, size_t bytes); // just use it :-)
+
 int main(void)
 {
     char *s1 = "Hello Graz!";
@@ -9,7 +11,20 @@ int main(void)
     strncpy(s2, "Hello World!", 12+1);    
 
     printf("s1 = \"%s\"\n", s1);
+    dump_memory(s1, 11+1);
+
     printf("s2 = \"%s\"\n", s2);
+    dump_memory(s1, 40+1);
 
     return 0;
+}
+
+void dump_memory(const char *s, size_t bytes)
+{
+    printf("memory [%p]: ", s);
+    for(size_t i=0; i<bytes; i++)
+    {
+        printf("%02x ", s[i]);
+    }
+    printf("\n");
 }
