40e4f51ab9a8116364e7a58cabc7a52141f0e0a4
Egon Teiniker <egon.teiniker@gmail.com>
2023-03-26 20:28:28 +0200

    Initial import.

---
+++ programming-c/c-advanced/memory-management/dynamic-arrays-triangular-exercise/dynamic_arrays_triangular.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int *array_triangular(size_t n);
+
+int expected[] = {1, 3, 6, 10, 15, 21, 28, 36, 45, 55};
+
+int main(void)
+{
+    int n = 10;
+
+    int *triangular = array_triangular(n);
+
+    for(int i=0; i<n; i++)
+    {
+        printf("triangular[%d] = %d\n", i, triangular[i]);
+        assert(triangular[i] == expected[i]);
+    }
+    
+    return 0;
+}
+
+// TODO: Implement array_triangular()
---
+++ programming-c/c-advanced/memory-management/dynamic-arrays-triangular/dynamic_arrays_triangular.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int *array_triangular(size_t n);
+
+int expected[] = {1, 3, 6, 10, 15, 21, 28, 36, 45, 55};
+
+int main(void)
+{
+    int n = 10;
+
+    int *triangular = array_triangular(n);
+
+    for(int i=0; i<n; i++)
+    {
+        printf("triangular[%d] = %d\n", i, triangular[i]);
+        assert(triangular[i] == expected[i]);
+    }
+    
+    free(triangular);
+
+    return 0;
+}
+
+int *array_triangular(size_t n)
+{
+    int *numbers = (int*)malloc(n * sizeof(int));
+    
+    for (int i = 0; i < n; i++) 
+    {
+        numbers[i] = (i+1)*(i+2)/2;
+    }
+    
+    return numbers;
+}
+
