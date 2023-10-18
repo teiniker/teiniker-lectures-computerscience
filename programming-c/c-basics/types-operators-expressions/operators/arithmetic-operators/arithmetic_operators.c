03656991b810addcda8aee0dd3a9a7fc3287dae7
teiniker <egon.teiniker@gmail.com>
2023-10-18 11:03:46 +0200

    Bugfix: arithmetic_operators.c

--- programming-c/c-basics/types-operators-expressions/operators/arithmetic-operators/arithmetic_operators.c
+++ programming-c/c-basics/types-operators-expressions/operators/arithmetic-operators/arithmetic_operators.c
@@ -9,23 +9,23 @@ int main(void)
     printf("a - b = %d\n", a-b);
     printf("a * b = %d\n", a*b);
     printf("a / b = %d\n", a/b);
-	printf("a % b = %d\n", a%b);    // Modulus (remainder after division)
+	printf("a %% b = %d\n", a%b);    // Modulus (remainder after division)
 
-    a++;        // increment a 
+    a++;        // increment a => a = a+1
     printf("a = %d\n", a);
 
     a += 10;    // a = a + 10
     printf("a = %d\n", a);
 
-    a--;        // decrement a
+    a--;        // decrement a => a = a-1
     printf("a = %d\n", a);
  
     a -= 5;    // a = a - 5
     printf("a = %d\n", a);
    
 
-    double x = 7.0f;
-    double y = 13.0f;
+    double x = 7.0;
+    double y = 13.0;
     printf("x + y = %f\n", x+y);
     printf("x - y = %f\n", x-y);
     printf("x * y = %f\n", x*y);
