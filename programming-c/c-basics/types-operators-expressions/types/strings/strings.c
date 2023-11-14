27420fcba98cb480ea63e34fb20cf642aeeda901
teiniker <egon.teiniker@fh-joanneum.at>
2023-11-14 08:19:14 +0100

    Refactoring: string.c

--- programming-c/c-basics/types-operators-expressions/types/strings/strings.c
+++ programming-c/c-basics/types-operators-expressions/types/strings/strings.c
@@ -6,17 +6,33 @@ void dump_memory(const void *s, size_t bytes); // just use it :-)
 
 int main(void)
 {
-    char *s1 = "Hello Graz\U0001F642!"; // ""
-    printf("s1 = \"%s\"\n", s1);
-    dump_memory(s1, strlen(s1)+1);
-    // memory [0x55cb409ea004]: 48 65 6c 6c 6f 20 47 72 61 7a f0 9f 99 82 21 00
-    // U+1F642 => UTF-8: f0 9f 99 82
+    {
+        char *s = "Hello Graz!"; 
+        printf("s = \"%s\"\n", s);
+        dump_memory(s, strlen(s)+1);
+        // s = "Hello Graz!"
+        // memory [0x5615c837e004]: 48 65 6c 6c 6f 20 47 72 61 7a 21 00
+    }
 
-    char s2[40+1];
-    strncpy(s2, "Hello World!", 12+1);    
-
-    printf("s2 = \"%s\"\n", s2);
-    dump_memory(s2, 40+1);
+    {
+        // Using Unicode escape sequences
+        char *s = "Hello Graz\U0001F642!"; 
+        printf("s = \"%s\"\n", s);
+        dump_memory(s, strlen(s)+1);
+        // s = "Hello Graz🙂!"
+        // memory [0x55cb409ea004]: 48 65 6c 6c 6f 20 47 72 61 7a f0 9f 99 82 21 00
+        // U+1F642 => UTF-8: f0 9f 99 82
+    }
+    
+    {
+        char s[40+1];
+        strncpy(s, "Hello World!", 12+1);    
+        printf("s = \"%s\"\n", s);
+        dump_memory(s, 40+1);
+        // s = "Hello World!"
+        // memory [0x7ffd6fe2b180]: 48 65 6c 6c 6f 20 57 6f 72 6c 64 21 00 00 00 00 
+        // 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 
+    }
 
     return 0;
 }
