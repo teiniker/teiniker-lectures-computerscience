981095001aabbf0237cd81e4375b16af2bc9c29d
teiniker <egon.teiniker@gmail.com>
2024-10-22 17:16:59 +0200

    Refactoring: Moved example.

--- programming-c/c-basics/types-operators-expressions/types/floating-point/floating-point-equal.c
+++ programming-c/c-basics/types-operators-expressions/types/floating-point/floating-point-equal.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <math.h>
-#include <assert.h>
 
 int main(void)
 {
@@ -8,8 +8,10 @@ int main(void)
     double actual = 3.7415;
     double error = 1E-3;
     
+    bool is_equal = fabs(expected-actual) <= error;
+    
     printf("expected: %11.10f \nactual  : %11.10f \nerror   : %11.10f\n", expected, actual, error);
-    assert(fabs(expected-actual) <= error);
+    printf("is_equal: %d\n", is_equal);
   
     return 0;
 }
