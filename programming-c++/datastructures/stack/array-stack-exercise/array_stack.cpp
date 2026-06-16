e9dfcecd5aa34d216f51ddb63e4af05e669c6d36
teiniker <egon.teiniker@gmail.com>
2026-06-16 16:27:02 +0200

    Added exercise.

---
+++ programming-c++/datastructures/stack/array-stack-exercise/array_stack.cpp
@@ -0,0 +1,7 @@
+#include <cstddef>
+#include <cstdlib>
+#include <cstdio>
+
+#include "array_stack.h"
+
+// TODO: Implement methods of the class
---
+++ programming-c++/datastructures/stack/array-stack-exercise/array_stack.h
@@ -0,0 +1,12 @@
+#ifndef _ARRAY_STACK_H_
+#define _ARRAY_STACK_H_
+
+#include <cstddef>
+
+
+class ArrayStack
+{
+    // TODO: Declare attributes and methods of the class
+};
+
+#endif /* _ARRAY_STACK_H_ */
